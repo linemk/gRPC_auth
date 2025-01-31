ec396b5dace507f95a1b20603f804f7f83205da6
maks <maks@MacBook-Air.local>
2025-02-01 00:46:43 +0300

    added slog and yaml

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,5 +1,43 @@
-package sso
+package main
+
+import (
+	"fmt"
+	"github.com/linemk/gRPC_auth/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}
 
 func main() {
+	cfg := config.MustLoad()
 
+	log := setupLogger(cfg.Env)
+	log.Info("starting application", slog.Any("env", cfg))
+
+	fmt.Println(cfg)
 }
