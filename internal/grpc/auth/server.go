9864ff14b71ab29d6ef6d518c945ecf48b471be6
maks <maks@MacBook-Air.local>
2025-02-01 16:00:14 +0300

    add interface

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -4,17 +4,38 @@ import (
 	"context"
 	ssov1 "github.com/linemk/proto_buf/gen/go/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+type Auth interface {
+	Login(ctx context.Context, email string, password string, appID int) (token string, err error)
+	RegisterNewUser(ctx context.Context, email string, password string) (userID int64, err error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
 
 type ServerApi struct {
 	ssov1.UnimplementedAuthServer
+	auth Auth
 }
 
-func Register(gRPC *grpc.Server) {
-	ssov1.RegisterAuthServer(gRPC, &ServerApi{})
+func Register(gRPC *grpc.Server, auth Auth) {
+	ssov1.RegisterAuthServer(gRPC, &ServerApi{auth: auth})
 }
+
+const (
+	emptyValue = 0
+)
 
 func (s *ServerApi) Login(ctx context.Context, req *ssov1.LoginRequest) (*ssov1.LoginResponse, error) {
+	if req.GetEmail() == "" || req.GetPassword() == "" {
+		return nil, status.Error(codes.InvalidArgument, "Email or password is required")
+	}
+
+	if req.GetAppId() == emptyValue {
+		return nil, status.Error(codes.InvalidArgument, "AppId is required")
+	}
+
 	return &ssov1.LoginResponse{
 		Token: req.GetEmail(),
 	}, nil
